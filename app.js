b25e6a132f46be34e2536650036d43637f92d646
Robson Muniz <robsonmuniz@Robsons-iMac.local>
2021-02-09 11:27:54 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const sounds = [
+  'applause',
+  'boo',
+  'cheer-crowd',
+  'tada',
+  'sad-trombone',
+  'snare-roll',
+];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+
+  btn.innerText = sound;
+
+  btn.addEventListener('click', () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById('buttons').appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+}
